1ebb7e52e3d0a381e3e98f5e4bff66e01b4b1cdd
Sasha Ivanov <arkseer@gmail.com>
2022-01-14 05:51:55 +0000

    [Update] Add condition to check if the arg passed through /twab includes a valid Bungie url

--- commands/twab.js
+++ commands/twab.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageActionRow, MessageButton, MessageSelectMenu } = require('discord.js');
-const { guild, core_roles, genders } = require('../config.json');
+const { guild, core_roles, genders, bungie_links } = require('../config.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -27,7 +27,12 @@ module.exports = {
         }
 
         if (getAdmin || getFounder) {
-            await interaction.reply({ content: `twab: ${getLink}`, ephemeral: true, components: [] });
+            if (getLink.includes(bungie_links['short']) || getLink.includes(bungie_links['long'])) {
+                await interaction.reply({ content: `twab: ${getLink}`, ephemeral: true, components: [] });
+            } else {
+                await interaction.reply({ content: `I'm terribly sorry${getGender}. But you need to provide me with a valid link to the twab.`, ephemeral: true, components: [] });
+            }
+            
         } else {
             await interaction.reply({ content: `I'm terribly sorry${getGender}! But you don't have permissions to use this command.`, ephemeral: true, components: [] });
         }
