c07ff4eb1c8002f4974e94dcbd528326d1b5cd1f
Sasha Ivanov <arkseer@gmail.com>
2022-01-15 09:51:15 +0000

    [Feature] Add /support command

---
+++ commands/support.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageActionRow, MessageButton, MessageSelectMenu, MessageEmbed } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('support')
+        .setDescription('Ways you can help support the community')
+        .setDefaultPermission(true),
+
+    async execute(interaction) {
+        await interaction.reply({ content: `Hello there!`, ephemeral: true, components: [] });
+    }
+}
