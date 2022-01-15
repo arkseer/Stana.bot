7b216a7f9b428221ed365a90d1877d01ff23a1c9
Sasha Ivanov <arkseer@gmail.com>
2022-01-15 10:01:29 +0000

    [Update] Add gender role check to address user properly. Change reply to /support cmd

--- commands/support.js
+++ commands/support.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageActionRow, MessageButton, MessageSelectMenu, MessageEmbed } = require('discord.js');
+const { genders } = require('../config.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -8,7 +9,16 @@ module.exports = {
         .setDefaultPermission(true),
 
     async execute(interaction) {
-        await interaction.reply({ content: `Hello there!`, ephemeral: true, components: [] });
+        let getGender;
+        // Check for gender roles to address user properly
+        if (interaction.member.roles.cache.some(role => role.id === genders['male'])) {
+            getGender = ", sir";
+        } else if (interaction.member.roles.cache.some(role => role.id === genders['female'])) {
+            getGender = ", ma'am";
+        } else {
+            getGender = "";
+        }
+        await interaction.reply({ content: `Hello there${getGender}! I've sent you a private message with further details on this.`, ephemeral: true, components: [] });
 
         // Send DM to user
         await interaction.user.send({ content: `Hello there!`, components: [] });
