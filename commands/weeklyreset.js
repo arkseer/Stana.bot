f091e6247d1dbc0125148ca0384b4e109648789b
Sasha Ivanov <arkseer@gmail.com>
2022-01-16 07:59:52 +0000

    [Update] Add timer to /weeklyreset message embed

--- commands/weeklyreset.js
+++ commands/weeklyreset.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageActionRow, MessageButton, MessageSelectMenu, MessageEmbed } = require('discord.js');
 const { genders } = require('../config.json');
+const { makeTimer } = require('../functions/makeTimer');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -18,9 +19,10 @@ module.exports = {
                 .addChoice('Swords of Legends Online', 'weekly_reset_solo'))),
 
     async execute(interaction) {
+        let timerD2 = makeTimer('22 January 2022 10:00:00 GMT-08:00')
         const countdownD2 = new MessageEmbed()
             .setColor('00cc96')
-            .setDescription('Next weekly reset is in **3 days**, **15 hours**, **40 minutes**.');
+            .setDescription(`Next weekly reset is in **3 days**, **15 hours**, **40 minutes**.\n${timerD2}`);
 
         await interaction.reply({ embeds: [countdownD2], ephemeral: true, components: [] });
     }
