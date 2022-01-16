685d0481fc0e695ea786baa6992d6e88b7544ae4
Sasha Ivanov <arkseer@gmail.com>
2022-01-16 07:30:47 +0000

    [Update] Remove the 2 subcommands and keep 1 for /weekly(reset). Add 2 options as string variables for d2 and solo.

--- commands/weeklyreset.js
+++ commands/weeklyreset.js
@@ -4,15 +4,18 @@ const { genders } = require('../config.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('weeklyreset')
-        .setDescription('Countdown until next weekly reset.')
+        .setName('weekly')
+        .setDescription('Countdown until next weekly reset')
         .setDefaultPermission(true)
         .addSubcommand(subcommand => subcommand
-            .setName('d2')
-            .setDescription('Countdown until next weekly reset in Destiny 2.'))
-        .addSubcommand(subcommand => subcommand
-            .setName('solo')
-            .setDescription('Countdown until next weekly reset in Swords of Legends Online.')),
+            .setName('reset')
+            .setDescription('Countdown until next weekly reset')
+            .addStringOption(option => option
+                .setName('game')
+                .setDescription('Countdown category you want to see')
+                .setRequired(true)
+                .addChoice('Destiny 2', 'weekly_reset_d2')
+                .addChoice('Swords of Legends Online', 'weekly_reset_solo'))),
 
     async execute(interaction) {
         await interaction.reply({ content: `weekly reset`, embeds: [], ephemeral: true, components: [] });
