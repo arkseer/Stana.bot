cbaa6373ae4b8970eea52f0cd27b88c2d5483f16
Sasha Ivanov <arkseer@gmail.com>
2022-01-16 08:01:49 +0000

    [Fix] Removed makeTimer.js from functions dir; Add makeTimer function to /weeklyreset file

--- commands/weeklyreset.js
+++ commands/weeklyreset.js
@@ -1,7 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageActionRow, MessageButton, MessageSelectMenu, MessageEmbed } = require('discord.js');
 const { genders } = require('../config.json');
-const { makeTimer } = require('../functions/makeTimer');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -19,6 +18,37 @@ module.exports = {
                 .addChoice('Swords of Legends Online', 'weekly_reset_solo'))),
 
     async execute(interaction) {
+        function makeTimer(endDate) {
+            var endTime = new Date(endDate);
+            endTime = Date.parse(endTime) / 1000;
+
+            var now = new Date();
+            now = Date.parse(now) / 1000;
+
+            var timeLeft = endTime - now;
+
+            var days = Math.floor(timeLeft / 86400);
+            var hours = Math.floor((timeLeft - days * 86400) / 3600);
+            var minutes = Math.floor((timeLeft - days * 86400 - hours * 3600) / 60);
+            var seconds = Math.floor(timeLeft - days * 86400 - hours * 3600 - minutes * 60);
+
+            if (hours < "10") {
+                hours = "0" + hours;
+            }
+            if (minutes < "10") {
+                minutes = "0" + minutes;
+            }
+            if (seconds < "10") {
+                seconds = "0" + seconds;
+            }
+
+            if (days < 1) {
+                return `${hours}h:${minutes}m:${seconds}s`;
+            } else {
+                return `${days}d:${hours}h:${minutes}m:${seconds}s`;
+            }
+        }
+        
         let timerD2 = makeTimer('22 January 2022 10:00:00 GMT-08:00')
         const countdownD2 = new MessageEmbed()
             .setColor('00cc96')
