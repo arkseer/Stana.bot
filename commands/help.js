d42bd7957043649f6dc8c045ac0efadf2177f00e
Sasha Ivanov <arkseer@gmail.com>
2022-01-17 08:19:52 +0000

    [Update] Add help commands and description to config.json

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageActionRow, MessageButton, MessageSelectMenu, MessageEmbed } = require('discord.js');
+const { help } = require('../config.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -8,6 +9,9 @@ module.exports = {
         .setDefaultPermission(true),
 
     async execute(interaction) {
+        const helpEmbed = new MessageEmbed()
+            .setColor('78282f')
+            .setDescription(`${help[0]} ${help[1]} ${help[2]}`);
         await interaction.reply({ content: `I'm sorry, but this feature is still under construction.`, ephemeral: true, components: [] });
     }
 }
