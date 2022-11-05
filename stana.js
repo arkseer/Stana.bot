5589016e92e03b2a843a2df5b290688295670284
Alexander Ivanov <arkseer@gmail.com>
2022-11-05 02:42:48 +0000

    Changed Token CVAR

--- stana.js
+++ stana.js
@@ -18,5 +18,5 @@ const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'
     }
     await client.handleEvents(eventFiles, '../events');
     await client.handleCommands(cmdFiles, '../commands');
-    await client.login(process.env.IMIHR_TOKEN);
+    await client.login(process.env.STANA_TOKEN);
 })();
