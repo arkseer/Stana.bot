9d61491abc335b2cfb00a1b87dcd730de26f615c
Alexander Ivanov <arkseer@gmail.com>
2022-11-06 05:20:56 +0000

    Working on voiceChannelGenerator

--- events/joinVoiceChannel.js
+++ events/joinVoiceChannel.js
@@ -12,36 +12,29 @@ module.exports = {
         const oldChannel = oldState.channel;
         const newChannel = newState.channel;
         const joinChannel = "1038644009381408788";
-        const guildID = "1038155332721066107";
-
-        if(guild.id == guildID) {
-            console.log(`same guild`);
-        } else {
-            console.log(`not in same guild`);
-        }
 
         if(oldChannel !== newChannel && newChannel && newChannel.id === joinChannel) {
             const voiceChannel = await guild.channels.create(`New channel #1`, {
                 type: "GUILD_VOICE",
-                parent: newChannel.parent,
-                permissionOverwrites: [
-                    {id: member.id, allow: [Permissions.FLAGS.CONNECT]},
-                    {id: guild.id, deny: [Permissions.FLAGS.CONNECT]}
-                ]
+                parent: newChannel.parent
             });
 
             client.voiceGenerator.set(member.id, voiceChannel.id);
+            /*
             await newChannel.permissionOverwrites.edit(member, {CONNECT: false});
             setTimeout(() => newChannel.permissionOverwrites.delete(member), 30 * 1000);
 
             return setTimeout(() => member.voice.setChannel(voiceChannel), 500);
+            */
         }
 
+        /*
         const ownedChannel = client.voiceGenerator.get(member.id);
 
         if(ownedChannel && oldChannel.id == ownedChannel && (!newChannel || newChannel.id !== ownedChannel)) {
             client.voiceGenerator.set(member.id, null);
             oldChannel.delete().catch(() => {});
         }
+        */
     }
 }
--- stana.js
+++ stana.js
@@ -7,11 +7,10 @@ const config = require('./config.json');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
 
 client.commands = new Collection();
+client.voiceGenerator = new Collection();
 const functions = fs.readdirSync('./functions').filter(file => file.endsWith('.js'));
 const cmdFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-
-client.voiceGenerator = new Collection();
 
 (async () => {    
     for (const file of functions) {
