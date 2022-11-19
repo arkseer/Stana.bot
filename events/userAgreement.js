ab7a8c1f928b8e53559b2759845606624c0a29dd
Alexander Ivanov <arkseer@gmail.com>
2022-11-19 07:26:11 +0000

    Changed channel ids, added extra level of verification when agreeing to terms

--- events/userAgreement.js
+++ events/userAgreement.js
@@ -8,9 +8,10 @@ module.exports = {
                 let userId = interaction.user.id;
                 let getGuild = await client.guilds.cache.get(guild);
                 let getMember = await getGuild.members.fetch(userId);
-                let getGender;
+                let getGender = "";
 
                 // Check for gender roles to address the user properly
+                /*
                 if (getMember.roles.cache.some(role => role.id === genders['male'])) {
                     getGender = ", sir";
                 } else if (getMember.roles.cache.some(role => role.id === genders['female'])) {
@@ -18,14 +19,17 @@ module.exports = {
                 } else {
                     getGender = "";
                 }
+                */
 
                 // Handle community default role
-                if (interaction.customId === 'user_agreement_btn') {
+                if (interaction.customId === 'user_agreement_btn' && (!getMember.roles.cache.some(role => role.id === core_roles['c_members']))) {
                     await interaction.reply({ content: `Great stuff${getGender}!\n\nPlease drop by <#${roles_channel_id}> and pick up any additional roles you may need to access other sections of the server.`, ephemeral: true, components: [] });
 
                     // Add default role to user
                     let getRole = getGuild.roles.cache.find(role => role.id === core_roles['c_members']);
                     await getMember.roles.add(getRole);
+                } else if(getMember.roles.cache.some(role => role.id === core_roles['c_members'])) {
+                    await interaction.reply({ content: `Sorry, but you already have accepted the terms.`, ephemeral: true, components: [] });
                 }
             } catch (error) {
                 console.error(error);
