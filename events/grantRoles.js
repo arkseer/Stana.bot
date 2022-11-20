f0fb350749d94d10b4d938f97efb52c0ef056bbb
Alexander Ivanov <arkseer@gmail.com>
2022-11-20 08:18:25 +0000

    Added functions to handle classes and agents roles

--- events/grantRoles.js
+++ events/grantRoles.js
@@ -27,6 +27,40 @@ module.exports = {
                         await getMember.roles.add(getRoles);
                     }
                 }
+
+                // Handle class roles
+                else if (interaction.customId === 'get_roles_va_classes') {
+                    await interaction.reply({ content: `You have been granted the selected role.`, ephemeral: true, components: [] });
+
+                    // Remove all class roles to prep the user when they re-select
+                    for (let x in roles.classes) {
+                        let getAllRoles = getGuild.roles.cache.find(role => role.id === roles.classes[x]['id']);
+                        await getMember.roles.remove(getAllRoles);
+                    }
+
+                    // Add selected roles to user
+                    for (let y of interaction.values) {
+                        let getRoles = getGuild.roles.cache.find(role => role.id === roles.classes[y]['id']);
+                        await getMember.roles.add(getRoles);
+                    }
+                }
+
+                // Handle agent roles
+                else if (interaction.customId === 'get_roles_va_agents') {
+                    await interaction.reply({ content: `You have been granted the selected role.`, ephemeral: true, components: [] });
+
+                    // Remove all agent roles to prep the user when they re-select
+                    for (let x in roles.agents) {
+                        let getAllRoles = getGuild.roles.cache.find(role => role.id === roles.agents[x]['id']);
+                        await getMember.roles.remove(getAllRoles);
+                    }
+
+                    // Add selected roles to user
+                    for (let y of interaction.values) {
+                        let getRoles = getGuild.roles.cache.find(role => role.id === roles.agents[y]['id']);
+                        await getMember.roles.add(getRoles);
+                    }
+                }
             } catch(error) {
                 console.error(error);
             }
