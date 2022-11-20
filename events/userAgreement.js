6ae09d750f5020ffaf2578dc37d3f6e87441a4e4
Alexander Ivanov <arkseer@gmail.com>
2022-11-20 07:34:22 +0000

    Grant the user all divider roles when they accept terms

--- events/userAgreement.js
+++ events/userAgreement.js
@@ -1,4 +1,4 @@
-const { guild, core_roles, genders, roles_channel_id } = require('../config.json');
+const { guild, dividers, core_roles, genders, roles_channel_id } = require('../config.json');
 
 module.exports = {
     name: 'interactionCreate',
@@ -24,6 +24,12 @@ module.exports = {
                 // Handle community default role
                 if (interaction.customId === 'user_agreement_btn' && (!getMember.roles.cache.some(role => role.id === core_roles['c_members']))) {
                     await interaction.reply({ content: `Great stuff${getGender}!\n\nPlease drop by <#${roles_channel_id}> and pick up any additional roles you may need to access other sections of the server.`, ephemeral: true, components: [] });
+
+                    // Add all dividers to user
+                    for (let roles in dividers) {
+                        let getAllDividers = getGuild.roles.cache.find(role => role.id === dividers[roles]['id']);
+                        await getMember.roles.add(getAllDividers);
+                    }
 
                     // Add default role to user
                     let getRole = getGuild.roles.cache.find(role => role.id === core_roles['c_members']);
