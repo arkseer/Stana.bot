6ce8308353b7f4caf32851bd4faf873c4e6c6f87
Alexander Ivanov <arkseer@gmail.com>
2022-11-26 10:25:51 +0000

    Changed welcome message and removed gender choice component

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -7,7 +7,7 @@ module.exports = {
         try {
             let role = member.guild.roles.cache.find(role => role.id === core_roles['n_members']);
 
-            const welcomeMessage = `Hello there!\nI am **Imihr**, **Enochian Gods**' personal butler and I will be your guide today.\n\nOn behalf of our humble community hub I would like to extend you a warm welcome.\nWe are honored to have someone of your status joining us, and I do hope you enjoy your time here.\n\nFirst I would like to address you properly in the future; please be kind to choose one of the following for me.`;
+            const welcomeMessage = `Hello there!\nI am **Stana**, **DMLC**'s secretary and I will be your guide today.\n\nOn behalf of our humble community hub I would like to extend you a warm welcome.\nWe are honored to have someone of your status joining us, and I do hope you enjoy your time here.\n\nFirst I would like to address you properly in the future; please be kind to choose one of the following for me.`;
             const genderChoice = new MessageActionRow()
                 .addComponents(
                     new MessageButton()
@@ -21,7 +21,7 @@ module.exports = {
                 );
 
             console.log(`[Log] A new member has joined! (${member.user.tag})`);
-            await member.send({ content: welcomeMessage, components: [genderChoice] });
+            await member.send({ content: welcomeMessage, components: [] });
             await member.roles.add(role);
         } catch (error) {
             console.error(error);
