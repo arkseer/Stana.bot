80701052378e029fd37dccfb1291109e37cba604
Alexander Ivanov <arkseer@gmail.com>
2022-11-26 18:51:41 +0000

    Stana.bot will send a message in the logs channel every time a new member joins.

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,11 +1,12 @@
 const { MessageActionRow, MessageButton, MessageSelectMenu } = require('discord.js');
-const { core_roles, welcome_channel_id } = require('../config.json');
+const { core_roles, welcome_channel_id, log_channel_id } = require('../config.json');
 
 module.exports = {
     name: 'guildMemberAdd',
     async execute(member) {
         try {
             let role = member.guild.roles.cache.find(role => role.id === core_roles['n_members']);
+            let getLogChannel = member.guild.channels.cache.get(log_channel_id);
             let getName = member.displayName;
 
             const welcomeMessage = `Hello there, ${getName}!\nMy name is **Stana** and I will be your guide today on behalf of the **DMLC** community.\n\nFirstly I would like to extend you a warm welcome, we are truly honored to have someone of your stature joining us.\nIt gives me great pleasure to make your acquaintance, and hope you enjoy your time here.\n\nTo complete your joining process and be granted access to the rest of the community, please navigate to the <#${welcome_channel_id}> channel to familiarize yourself with our rules.`;
@@ -24,6 +25,7 @@ module.exports = {
             console.log(`[Log] A new member has joined! (${member.user.tag} + ${member.user.username})`);
             await member.send({ content: welcomeMessage, components: [] });
             await member.roles.add(role);
+            await getLogChannel.send({ content: `[Log] [+] ${member.user.tag} has joined the server.`, components: [] });
         } catch (error) {
             console.error(error);
         }
