cee39681773b5fbf8e550f912f01789b75659d8e
Alexander Ivanov <arkseer@gmail.com>
2022-11-26 18:11:50 +0000

    Changed welcome message when new member joins + added welcome channel id to config.json

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,13 +1,14 @@
 const { MessageActionRow, MessageButton, MessageSelectMenu } = require('discord.js');
-const { core_roles } = require('../config.json');
+const { core_roles, welcome_channel_id } = require('../config.json');
 
 module.exports = {
     name: 'guildMemberAdd',
     async execute(member) {
         try {
             let role = member.guild.roles.cache.find(role => role.id === core_roles['n_members']);
+            let getName = member.displayName();
 
-            const welcomeMessage = `Hello there!\nI am **Stana**, **DMLC**'s secretary and I will be your guide today.\n\nOn behalf of our humble community hub I would like to extend you a warm welcome.\nWe are honored to have someone of your status joining us, and I do hope you enjoy your time here.\n\nFirst I would like to address you properly in the future; please be kind to choose one of the following for me.`;
+            const welcomeMessage = `Hello there, ${getName}!\nMy name is **Stana** and I will be your guide today on behalf of the **DMLC** community.\n\nFirstly I would like to extend you a warm welcome, we are truly honored to have someone of your stature joining us.\nIt gives me great pleasure to make your acquaintance, and hope you enjoy your time here.\n\nTo complete your joining process and be granted access to the rest of the community, please navigate to the <#${welcome_channel_id}> channel to familiarize yourself with our rules.`;
             const genderChoice = new MessageActionRow()
                 .addComponents(
                     new MessageButton()
@@ -20,7 +21,7 @@ module.exports = {
                         .setStyle('SECONDARY'),
                 );
 
-            console.log(`[Log] A new member has joined! (${member.user.tag})`);
+            console.log(`[Log] A new member has joined! (${member.user.tag} + ${member.user.username})`);
             await member.send({ content: welcomeMessage, components: [] });
             await member.roles.add(role);
         } catch (error) {
