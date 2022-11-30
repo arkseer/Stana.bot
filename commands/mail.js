439a33de045311bc1b681c5f23587a7f01fb50ab
Alexander Ivanov <arkseer@gmail.com>
2022-11-30 18:21:33 +0000

    Added new command /mail

---
+++ commands/mail.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { guild } = require('../config.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('mail')
+        .setDescription('Mail a moderator to assist with your issue')
+        .addSubcommand(subcommand => subcommand
+            .setName('me')
+            .setDescription('Mail a moderator to assist with your issue')
+            .addStringOption(option => option
+                .setName('priority')
+                .setDescription('Select the priority of your issue')
+                .setRequired(true)
+                .addChoice('High', 'high')
+                .addChoice('Normal', 'normal')
+                .addChoice('Low', 'low'))
+            .addStringOption(option => option
+                .setName('issue')
+                .setDescription('Describe your issue as best you can')
+                .setRequired(true))
+        ),
+
+    async execute(interaction, client) {
+        interaction.reply({ content: `it works`, ephemeral: true, components: [] });
+    }
+}
