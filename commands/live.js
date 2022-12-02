197566fd9ea87d503adad5352aff54f097c904cc
Alexander Ivanov <arkseer@gmail.com>
2022-12-02 19:00:37 +0000

    Added new command: /live

---
+++ commands/live.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const { guild } = require('../config.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('live')
+        .setDescription('Send a notification when you go live or apply to join our content creators programme')
+        .addSubcommand(subcommand => subcommand
+            .setName('@me')
+            .setDescription('Send a notification when you go live'))
+        .addSubcommand(subcommand => subcommand
+            .setName('apply')
+            .setDescription('Apply to join our content creators programme')),
+
+    async execute(interaction, client) {
+        const wait = require('util').promisify(setTimeout);
+
+        interaction.reply({ content: `it works`, ephemeral: true, components: [] });
+    }
+}
