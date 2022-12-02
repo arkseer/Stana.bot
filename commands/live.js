3a7bc125c249e7403ce704fa0e25969ac300c286
Alexander Ivanov <arkseer@gmail.com>
2022-12-02 19:11:56 +0000

    Removed 2nd subcommand and changed subcommand @me to now

--- commands/live.js
+++ commands/live.js
@@ -5,13 +5,10 @@ const { guild } = require('../config.json');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('live')
-        .setDescription('Send a notification when you go live or apply to join our content creators programme')
+        .setDescription('Send a notification when you go live')
         .addSubcommand(subcommand => subcommand
-            .setName('\@me')
-            .setDescription('Send a notification when you go live'))
-        .addSubcommand(subcommand => subcommand
-            .setName('apply')
-            .setDescription('Apply to join our content creators programme')),
+            .setName('now')
+            .setDescription('Send a notification when you go live')),
 
     async execute(interaction, client) {
         const wait = require('util').promisify(setTimeout);
