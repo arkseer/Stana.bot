ec0c6c7e2d29fb04b149b355cb41e2224d9c0f40
Alexander Ivanov <arkseer@gmail.com>
2022-12-03 00:16:47 +0000

    Added 'title' for stream, changed liveEmbed() description and color

--- commands/live.js
+++ commands/live.js
@@ -10,6 +10,10 @@ module.exports = {
             .setName('now')
             .setDescription('Send a notification when you go live')
             .addStringOption(option => option
+                .setName('title')
+                .setDescription('Write the title of your stream')
+                .setRequired(true))
+            .addStringOption(option => option
                 .setName('platform')
                 .setDescription('Choose the platform you are live on')
                 .setRequired(true)
@@ -23,8 +27,8 @@ module.exports = {
         function liveEmbed() {
             const livePost = new MessageEmbed()
                 .setAuthor({ name: interaction.member.displayName, url: `https://dmlc.store`, iconURL: interaction.user.displayAvatarURL() })
-                .setColor('cf889f')
-                .setDescription('Description goes here')
+                .setColor('9b59b6')
+                .setDescription(`"Title goes here !blank !discord" — LIVE on Twitch`)
                 .setImage('https://i.imgur.com/9b10bBR.png');
 
             return livePost;
