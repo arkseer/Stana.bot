d0a7dca8bebe2e26252bd08bb2877a0e94e7b738
Alexander Ivanov <arkseer@gmail.com>
2022-12-04 17:55:32 +0000

    Created new command /apply and layed out the basic structure

---
+++ commands/apply.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const { guild, bot } = require('../config.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('apply')
+        .setDescription('Apply to different programmes the community has to offer'),
+
+    async execute(interaction) {
+        const wait = require('util').promisify(setTimeout);
+        const getBot = interaction.guild.members.fetch(bot);
+
+        function applyEmbed() {
+            const applyPost = new MessageEmbed()
+                .setAuthor({ name: `\u2800`, url: ``, iconURL: getBot.displayAvatarURL() })
+                .setColor('cf889f')
+                .setDescription('Description goes here')
+                .setImage('https://i.imgur.com/tGSh027.png');
+
+            return applyPost;
+        }
+
+        let _applyEmbed = applyEmbed();
+
+        await interaction.reply({ content: `it works`, ephemeral: true, components: [], embeds: [_applyEmbed] });
+    }
+
+}
