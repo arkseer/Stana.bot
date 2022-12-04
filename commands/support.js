c3c7cd661371735e8c830661c3896fde0f85e7be
Alexander Ivanov <arkseer@gmail.com>
2022-12-04 17:11:37 +0000

    Changed structure of /support, added supportEmbed() and supportBtns

--- commands/support.js
+++ commands/support.js
@@ -1,26 +1,41 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageActionRow, MessageButton, MessageSelectMenu, MessageEmbed } = require('discord.js');
-const { genders } = require('../config.json');
+const { bot } = require('../config.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('support')
-        .setDescription('Ways you can help support the community')
-        .setDefaultPermission(true),
+        .setDescription('Ways you can help support the community'),
 
     async execute(interaction) {
-        let getGender;
-        // Check for gender roles to address user properly
-        if (interaction.member.roles.cache.some(role => role.id === genders['male'])) {
-            getGender = ", sir";
-        } else if (interaction.member.roles.cache.some(role => role.id === genders['female'])) {
-            getGender = ", ma'am";
-        } else {
-            getGender = "";
+        const wait = require('util').promisify(setTimeout);
+        const getBot = await interaction.guild.members.fetch(bot);
+
+        function supportEmbed() {
+            const supportPost = new MessageEmbed()
+                .setAuthor({ name: `\u2800`, url: ``, iconURL: getBot.displayAvatarURL() })
+                .setColor('cf889f')
+                .setDescription(`Description goes here`)
+                .setImage('https://i.imgur.com/m1p36YF.png');
         }
-        await interaction.reply({ content: `Hello there${getGender}! I've sent you a private message with further details on this.`, ephemeral: true, components: [] });
+
+        const supportBtn = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setLabel('Patreon')
+                    .setStyle('LINK')
+                    .setURL('https://dmlc.store'),
+                new MessageButton()
+                    .setLabel('Merch store')
+                    .setStyle('LINK')
+                    .setURL('https://dmlc.store'),
+            );
+
+        let _supportEmbed = supportEmbed();
+
+        await interaction.reply({ content: `it works`, ephemeral: true, components: [supportBtn], embeds: [_supportEmbed] });
 
         // Send DM to user
-        await interaction.user.send({ content: `Hello${getGender}! At this moment the best way to support our community is through boosts.\nKeep an eye on future updates if you wish to show your support through different avenues.\n\nThank you!`, components: [] });
+        //await interaction.user.send({ content: `Hello${getGender}! At this moment the best way to support our community is through boosts.\nKeep an eye on future updates if you wish to show your support through different avenues.\n\nThank you!`, components: [] });
     }
 }
