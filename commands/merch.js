4ecd3712061c4d3a6a5396cdc449ebd169bcf813
Alexander Ivanov <arkseer@gmail.com>
2022-12-04 14:59:06 +0000

    Removed author name and url

--- commands/merch.js
+++ commands/merch.js
@@ -14,7 +14,7 @@ module.exports = {
 
         function merchEmbed() {
             const merchPost = new MessageEmbed()
-                .setAuthor({ name: getBot.displayName, url: url.merch.link, iconURL: getBot.displayAvatarURL() })
+                .setAuthor({ name: ``, url: ``, iconURL: getBot.displayAvatarURL() })
                 .setColor('cf889f')
                 .setDescription(`Description goes here`)
                 .setImage('https://i.imgur.com/y0bAZc3.png');
