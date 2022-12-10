2a9fb022e253b1cf54198906c9c685dc8cf05226
Alexander Ivanov <arkseer@gmail.com>
2022-12-10 04:43:13 +0000

    Granting role if member agrees to rules

--- events/applyInteraction.js
+++ events/applyInteraction.js
@@ -1,12 +1,16 @@
 const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
-const { bot } = require('../config.json');
+const { bot, dividers, roles: { applications } } = require('../config.json');
 
 module.exports = {
     name: 'interactionCreate',
     async execute(interaction, client) {
         if (interaction.isButton()) {
             try {
+                let userId = interaction.user.id;
+                let getGuild = await interaction.guild;
+                let getMember = await interaction.guild.members.fetch(userId);
                 const getBot = await interaction.guild.members.fetch(bot);
+
                 // code goes here
                 if (interaction.customId === 'apply_cc') {
                     function applyEmbed() {
@@ -34,6 +38,10 @@ module.exports = {
                     
                     await interaction.reply({ ephemeral: true, components: [applyBtn], embeds: [applyEmbed()] });
                 } else if (interaction.customId === 'apply_cc_agree') {
+                    let applyRole = getGuild.roles.cache.find(role => role.id === applications['content_creator'][id]);
+                    
+                    await getMember.roles.add(applyRole);
+
                     await interaction.reply({ content: `i agree`, ephemeral: true, components: [], embeds: [] });
                 } else if (interaction.customId === 'apply_cc_disagree') {
                     await interaction.reply({ content: `i disagree`, ephemeral: true, components: [], embeds: [] });
