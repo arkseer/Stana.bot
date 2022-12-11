2a16c0d0520fa5dcbeae055c09eb54b704d1e816
Alexander Ivanov <arkseer@gmail.com>
2022-12-11 09:11:43 +0000

    Added block for 'editor' application interaction

--- events/applyInteraction.js
+++ events/applyInteraction.js
@@ -11,7 +11,7 @@ module.exports = {
                 let getMember = await interaction.guild.members.fetch(userId);
                 const getBot = await interaction.guild.members.fetch(bot);
 
-                // code goes here
+                // Content creator
                 if (interaction.customId === 'apply_cc') {
                     function applyEmbed() {
                         const embedDescription = `Before we continue, there are a few things we need to go over\n to assure we form a great partnership benefiting both of us.\nWith you operating under our banner, we would like to keep things on a professional level and maintain a healthy level of interest.\n\nYou agreeing to partner with us, you represent the **community** indirectly and therefore adhere to our rules to ensure\n we develop a healthy environment for our audience.`;
@@ -47,6 +47,43 @@ module.exports = {
                     await interaction.reply({ content: `Moving onto next phase of this process, please use the **/cc** command and attach all details to support your application.\nThank you for understanding.`, ephemeral: true, components: [], embeds: [] });
                 } else if (interaction.customId === 'apply_cc_disagree') {
                     await interaction.reply({ content: `Thank you for showing interest in our Content creator programme, please feel free to follow this process at a later date if you reconsider your choice.`, ephemeral: true, components: [], embeds: [] });
+                }
+                // Editor
+                if (interaction.customId === 'apply_editor') {
+                    function applyEmbed() {
+                        const embedDescription = `Before we continue, there are a few things we need to go over\n to assure we form a great partnership benefiting both of us.\nWith you operating under our banner, we would like to keep things on a professional level and maintain a healthy level of interest.\n\nYou agreeing to partner with us, you represent the **community** indirectly and therefore adhere to our rules to ensure\n we develop a healthy environment for our audience.`;
+                        const applyPost = new MessageEmbed()
+                            .setAuthor({ name: `\u2800`, url: ``, iconURL: getBot.displayAvatarURL() })
+                            .setColor('cf889f')
+                            .setDescription(embedDescription)
+                            .setImage('https://i.imgur.com/tGSh027.png');
+                        
+                        return applyPost;
+                    }
+
+                    const applyBtn = new MessageActionRow()
+                        .addComponents(
+                            new MessageButton()
+                                .setCustomId('apply_editor_agree')
+                                .setLabel('I agree')
+                                .setStyle('SUCCESS'),
+                            new MessageButton()
+                                .setCustomId('apply_editor_disagree')
+                                .setLabel('I disagree')
+                                .setStyle('DANGER'),
+                        );
+                    
+                    await interaction.reply({ ephemeral: true, components: [applyBtn], embeds: [applyEmbed()] });
+                } else if (interaction.customId === 'apply_editor_agree') {
+                    let applyDivider = getGuild.roles.cache.find(role => role.id === dividers.applications.id);
+                    let applyRole = getGuild.roles.cache.find(role => role.id === applications.pending.editor.id);
+                    
+                    await getMember.roles.add(applyDivider);
+                    await getMember.roles.add(applyRole);
+
+                    await interaction.reply({ content: `Moving onto next phase of this process, please use the **/editor** command and attach all details to support your application.\nThank you for understanding.`, ephemeral: true, components: [], embeds: [] });
+                } else if (interaction.customId === 'apply_editor_disagree') {
+                    await interaction.reply({ content: `Thank you for showing interest in our Editor programme, please feel free to follow this process at a later date if you reconsider your choice.`, ephemeral: true, components: [], embeds: [] });
                 }
             } catch (error) {
                 console.error(error);
