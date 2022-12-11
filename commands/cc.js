cd79b16f0d2136288a69f77e9277f06d25e24139
Alexander Ivanov <arkseer@gmail.com>
2022-12-11 07:28:09 +0000

    Added options to /cc command

--- commands/cc.js
+++ commands/cc.js
@@ -2,9 +2,25 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { bot } = require('../config.json');
 
 module.exports = {
+    // Command format: /cc <linktree> <platform> <link>
     data: new SlashCommandBuilder()
         .setName('cc')
-        .setDescription('Submit details to support your Content creator application'),
+        .setDescription('Submit details to support your Content creator application')
+        .addStringOption(option => option
+            .setName('linktree')
+            .setDescription('Enter your linktr.ee link (for social media platforms)')
+            .setRequired(true)
+        .addStringOption(option => option
+            .setName('platform')
+            .setDescription('Choose the main platform you publish your content on')
+            .setRequired(true)
+            .addChoice('Twitch', 'twitch')
+            .addChoice('Youtube', 'youtube')
+            .addChoice('Tiktok', 'tiktok'))
+        .addStringOption(option => option
+            .setName('link')
+            .setDescription('Enter the link to your main platform')
+            .setRequired(true))),
 
     async execute(interaction) {
         await interaction.reply({ content: `it works`, ephemeral: true, components: [] });
