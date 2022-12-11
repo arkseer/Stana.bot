de1de89d20fbfb0b2630fe72402fc21d858a0492
Alexander Ivanov <arkseer@gmail.com>
2022-12-11 07:37:31 +0000

    Added ccEmbed and ccBtn to be used for later when sending application details to staff only channel

--- commands/cc.js
+++ commands/cc.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { bot } = require('../config.json');
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const { bot, channels: { management } } = require('../config.json');
 
 module.exports = {
     // Command format: /cc <linktree> <platform> <link>
@@ -23,6 +24,35 @@ module.exports = {
             .setRequired(true))),
 
     async execute(interaction) {
-        await interaction.reply({ content: `it works`, ephemeral: true, components: [] });
+        const wait = require('util').promisify(setTimeout);
+        const getBot = await interaction.guild.members.fetch(bot);
+
+        function ccEmbed() {
+            const embedDescription = `some description`;
+
+            const ccPost = new MessageEmbed()
+                .setAuthor({ name: `\u2800`, url: ``, iconURL: getBot.displayAvatarURL() })
+                .setColor('cf889f')
+                .setDescription(embedDescription)
+                .setImage('https://i.imgur.com/tGSh027.png');
+
+            return ccPost;
+        }
+
+        const ccBtn = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setCustomId('cc_app_approve')
+                    .setLabel('Approve')
+                    .setStyle('SUCCESS'),
+                new MessageButton()
+                    .setCustomId('cc_app_deny')
+                    .setLabel('Deny')
+                    .setStyle('DANGER'),
+            );
+
+        let _ccEmbed = ccEmbed();
+
+        await interaction.reply({ content: `it works`, ephemeral: true, components: [ccBtn], embeds: [_ccEmbed] });
     }
 }
