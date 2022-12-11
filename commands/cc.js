4d6bdb5d80e1d2837381cba92027d894810c4f8b
Alexander Ivanov <arkseer@gmail.com>
2022-12-11 07:55:27 +0000

    Added fields and changed description of ccEmbed

--- commands/cc.js
+++ commands/cc.js
@@ -9,7 +9,7 @@ module.exports = {
         .setDescription('Submit details to support your Content creator application')
         .addStringOption(option => option
             .setName('linktree')
-            .setDescription('Enter your linktr.ee link (for social media platforms)')
+            .setDescription('Enter your linktr.ee link (for social media platforms) (provide full url)')
             .setRequired(true))
         .addStringOption(option => option
             .setName('platform')
@@ -20,7 +20,7 @@ module.exports = {
             .addChoice('Tiktok', 'tiktok'))
         .addStringOption(option => option
             .setName('link')
-            .setDescription('Enter the link to your main platform')
+            .setDescription('Enter the link to your main platform (provide full url)')
             .setRequired(true)),
 
     async execute(interaction) {
@@ -28,12 +28,15 @@ module.exports = {
         const getBot = await interaction.guild.members.fetch(bot);
 
         function ccEmbed() {
-            const embedDescription = `some description`;
+            const embedDescription = `**CONTENT CREATOR APPLICATION: ${interaction.member.displayName}**\n\nApplicant's details are as follows:\n`;
 
             const ccPost = new MessageEmbed()
                 .setAuthor({ name: `\u2800`, url: ``, iconURL: getBot.displayAvatarURL() })
-                .setColor('cf889f')
+                .setColor('6d6085')
                 .setDescription(embedDescription)
+                .addField(`Main platform`, `platform here`, false)
+                .addField(`Main platform url`, `link here`, false)
+                .addField(`Linktr.ee url`, `url here`, false)
                 .setImage('https://i.imgur.com/tGSh027.png');
 
             return ccPost;
