53e84afed439e79feaa34c9078902e356d873cb6
Alexander Ivanov <arkseer@gmail.com>
2022-12-11 09:37:33 +0000

    Added block for designer application interaction

--- events/applyInteraction.js
+++ events/applyInteraction.js
@@ -85,6 +85,43 @@ module.exports = {
                 } else if (interaction.customId === 'apply_editor_disagree') {
                     await interaction.reply({ content: `Thank you for showing interest in our Editor programme, please feel free to follow this process at a later date if you reconsider your choice.`, ephemeral: true, components: [], embeds: [] });
                 }
+                // Designer
+                if (interaction.customId === 'apply_designer') {
+                    function applyEmbed() {
+                        const embedDescription = `Before we continue, there are a few things we need to go over\n to assure we form a great partnership benefiting both of us.\nWith you operating under our banner, we would like to keep things on a professional level and maintain a healthy level of interest.\n\nYou agreeing to partner with us, you represent the **community** indirectly and therefore adhere to our rules to ensure\n we develop a healthy environment for our audience.`;
+                        const applyPost = new MessageEmbed()
+                            .setAuthor({ name: `\u2800`, url: ``, iconURL: getBot.displayAvatarURL() })
+                            .setColor('cf889f')
+                            .setDescription(embedDescription)
+                            .setImage('https://i.imgur.com/tGSh027.png');
+                        
+                        return applyPost;
+                    }
+
+                    const applyBtn = new MessageActionRow()
+                        .addComponents(
+                            new MessageButton()
+                                .setCustomId('apply_designer_agree')
+                                .setLabel('I agree')
+                                .setStyle('SUCCESS'),
+                            new MessageButton()
+                                .setCustomId('apply_designer_disagree')
+                                .setLabel('I disagree')
+                                .setStyle('DANGER'),
+                        );
+                    
+                    await interaction.reply({ ephemeral: true, components: [applyBtn], embeds: [applyEmbed()] });
+                } else if (interaction.customId === 'apply_designer_agree') {
+                    let applyDivider = getGuild.roles.cache.find(role => role.id === dividers.applications.id);
+                    let applyRole = getGuild.roles.cache.find(role => role.id === applications.pending.designer.id);
+                    
+                    await getMember.roles.add(applyDivider);
+                    await getMember.roles.add(applyRole);
+
+                    await interaction.reply({ content: `Moving onto next phase of this process, please use the **/designer** command and attach all details to support your application.\nThank you for understanding.`, ephemeral: true, components: [], embeds: [] });
+                } else if (interaction.customId === 'apply_designer_disagree') {
+                    await interaction.reply({ content: `Thank you for showing interest in our Editor programme, please feel free to follow this process at a later date if you reconsider your choice.`, ephemeral: true, components: [], embeds: [] });
+                }
             } catch (error) {
                 console.error(error);
             }
