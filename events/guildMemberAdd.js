b456982a4d2ca36b961ced2a640181219f2fb00f
Alexander Ivanov <arkseer@gmail.com>
2022-12-25 16:43:21 +0000

    Changed guildMemberAdd event to detect if new members are bots or people

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -6,8 +6,10 @@ module.exports = {
     async execute(member) {
         try {
             let role = member.guild.roles.cache.find(role => role.id === core_roles['n_members']);
+            let botRole = member.guild.roles.cache.find(role => role.id === core_roles.bot);
             let getLogChannel = member.guild.channels.cache.get(log_channel_id);
             let getName = member.displayName;
+            let isBot = member.user.bot;
 
             const welcomeMessage = `Hello there, ${getName}!\nMy name is **Stana** and I will be your guide today on behalf of the **DMLC** community.\n\nFirstly I would like to extend you a warm welcome, we are truly honored to have someone of your stature joining us.\nIt gives me great pleasure to make your acquaintance, and hope you enjoy your time here.\n\nTo complete your joining process and be granted access to the rest of the community, please navigate to the <#${welcome_channel_id}> channel to familiarize yourself with our rules.`;
             const genderChoice = new MessageActionRow()
@@ -23,9 +25,14 @@ module.exports = {
                 );
 
             console.log(`[Log] A new member has joined! (${member.user.tag} + ${member.user.username})`);
-            await member.send({ content: welcomeMessage, components: [] });
-            await member.roles.add(role);
-            await getLogChannel.send({ content: `[Log] [+] ${member.user.tag} has joined the server.`, components: [] });
+            if (isBot) {
+                await member.roles.add(botRole);
+                await getLogChannel.send({ content: `[Log] [+] ${member.user.tag} has joined the server.`, components: [] });
+            } else {
+                await member.send({ content: welcomeMessage, components: [] });
+                await member.roles.add(role);
+                await getLogChannel.send({ content: `[Log] [+] ${member.user.tag} has joined the server.`, components: [] });
+            }
         } catch (error) {
             console.error(error);
         }
